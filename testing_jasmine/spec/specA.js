94cd005730a44386be8a5913c41831c209ff3b0e
Martin Broesamle <m@minusbroe.de>
2017-07-19 10:22:27 +0200

    Initial: Test array creation with new.

---
+++ testing_jasmine/spec/specA.js
@@ -0,0 +1,7 @@
+describe("Array", function() {
+    it("can be created using `new`", function () {
+        var arr = new Array();
+        expect(arr instanceof Array).toBe(true)
+    });    
+});
+
