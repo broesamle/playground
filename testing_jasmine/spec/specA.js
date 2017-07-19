1041525790b5cf5e124e071d80b1c1965c6343fe
Martin Broesamle <m@minusbroe.de>
2017-07-19 10:45:18 +0200

    Describe the functionality of push.

--- testing_jasmine/spec/specA.js
+++ testing_jasmine/spec/specA.js
@@ -1,7 +1,22 @@
 describe("Array", function() {
+    var arr;
     it("can be created using `new`", function () {
-        var arr = new Array();
+        arr = new Array();
         expect(arr instanceof Array).toBe(true)
-    });    
+        expect(arr.length).toBe(0);
+    });
+    describe("push", function() {
+        it("adds an element", function () {
+            arr.push("element one");
+            expect(arr.length).toBe(1);
+            expect(arr[0]).toBe("element one");
+        });
+        it("inserts new elements at the end (highest index)", function () {
+            arr.push("element two");
+            expect(arr.length).toBe(2);
+            expect(arr[0]).toBe("element one");
+            expect(arr[1]).toBe("element two");
+        });
+    });
 });
 
